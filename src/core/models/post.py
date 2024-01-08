5534c01bb0c935cc57baf8c08539650cee2f19c2
nikolay <Nikc-mk@mail.ru>
2024-01-08 21:22:37 +1000

    create profile for user, migration for profile

--- src/core/models/post.py
+++ src/core/models/post.py
@@ -1,10 +1,13 @@
-from sqlalchemy import ForeignKey, String, Text
+from sqlalchemy import String, Text
 from sqlalchemy.orm import Mapped, mapped_column
 
 from src.core.models.base import Base
+from src.core.models.mixins import UserRelationMixin
 
 
-class Post(Base):
+class Post(UserRelationMixin, Base):
+    _user_id_nullable = False
+    _user_id_unique = False
+    _user_back_populates = "posts"
     title: Mapped[str] = mapped_column(String(100))
     body: Mapped[str] = mapped_column(Text, default="", server_default="")
-    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
--- src/core/models/user.py
+++ src/core/models/user.py
@@ -1,8 +1,16 @@
+from typing import TYPE_CHECKING
+
 from sqlalchemy import String
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from src.core.models.base import Base
+
+if TYPE_CHECKING:
+    from src.core.models.post import Post
+    from src.core.models.profile import Profile
 
 
 class User(Base):
     user_name: Mapped[str] = mapped_column(String(32), unique=True)
+    posts: Mapped[list["Post"]] = relationship(back_populates="user")
+    profile: Mapped["Profile"] = relationship(back_populates="user")
